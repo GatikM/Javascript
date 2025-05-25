28f8976d1605b5e69aedf9500283421b1767670c
GatikM <gatikmadaan1809@gmail.com>
2025-05-26 01:05:35 +0530

    basics of variables

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 1809;
+let accountName = "Garry Rhodes";
+var accountAge = 18;
+accountDesignation = "Software Engineer";
+const accountSalary = 39000;
+
+console.log(accountId);
+
+//accountName = "Garry Rhodes Junior";
+accountAge = 21;
+accountDesignation = "Data Analyst";
+
+console.table({accountId, accountName, accountAge, accountDesignation, accountSalary});
